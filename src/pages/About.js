eab23df65d600f6121f87870b5ab60d03f3d5d5b
whn2007 <whn2007@nyu.edu>
2022-06-08 14:33:04 -0700

    updates to homepage

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,10 @@
 import React from 'react'
-import {MenuList} from "../helpers/MenuList"
-import MenuItem from "../components/MenuItem"
 import "../styles/About.css"
 
 function About() {
   return (
     <div className='about'>
-        <h1 className='aboutTitle'>Our Team</h1>
-        <div className='aboutList'>
-            {MenuList.map((menuItem, key) => {
-                return <MenuItem
-                  key = {key}
-                  image={menuItem.image}
-                  name={menuItem.name} 
-                  description={menuItem.description} 
-                  link={menuItem.link} 
-                  />
-            })}
-        </div>
+        
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import {Link} from "react-router-dom"
-import BannerImage from "../assets/namebaselogo.png"
+import firstBlockImage from "../assets/firstBlock.png"
 import "../styles/Home.css"
 
 
 function Home() {
   return (
-    <div className='home' 
-      style={{backgroundImage: `url(${BannerImage})`,
+    <div className='firstBlock' 
+      style={{
+      backgroundColor: 'black',
+      backgroundImage: `url(${firstBlockImage})`,
       backgroundRepeat: "no-repeat",
       }}>
-        <div className='headerContainer'>
-          <Link to = "/mission">
-            <button>OUR MISSION</button>
-          </Link>
+        <div className='firstBlockContainer'>
+          <h1>A HANDSHAKE IMPROVEMENT <br/> PROPOSAL TO FIX</h1>
+          <h1>TOKEN ECONOMICS</h1>
+        </div>
+
+        <div className = "firstBlockSecondContainer">
+          <div className='firstHalf'>
+            <h1>Change is coming</h1>
+            <h2>Supported by the <br/> Handshake founders</h2>
+            <p>Handshake is the world's most powerful <br/>
+             peer to peer root naming system.</p>
+            <Link to = "/mission">
+              <button>Get Involved {'>'}</button>
+            </Link>
+          </div>
+          <div className='secondHalf'>
+            <h1>PEER-TO-PEER <br/> ROOT NAMING <br/> SYSTEM</h1>
+          </div>
+        </div>
+
+        <div className='firstBlockThirdContainer'>
+
         </div>
     </div>
+
   )
 }
 
