b18bcfce57c50922d7b3c05b915cc8c2d19fa31a
whn2007 <whn2007@nyu.edu>
2022-06-08 21:07:00 -0700

    push so if there's bugs i can revert to this

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,11 @@ import "../styles/About.css"
 function About() {
   return (
     <div className='about'>
-        
+        <div className='aboutHeader'>
+          <h1>THE FOUNDERS</h1>
+          <h2>Handshake is the world's most powerful
+            peer-to-peer root naming system.</h2>
+        </div>
     </div>
   )
 }
