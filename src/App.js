ac7e32faf3f958b0d7c955ad4a7cc6398e9bfc55
whn2007 <whn2007@nyu.edu>
2022-06-09 22:56:10 -0700

    scrolling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop"
 import Home from "./pages/Home";
 import Mission from "./pages/Mission";
 import About from "./pages/About";
@@ -10,12 +11,14 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <ScrollToTop/>
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/namer-community" exact element={<Home />} />
           <Route path="/mission" exact element={<Mission />} />
           <Route path="/about" exact element={<About />} />
+          <Route path="*" exact element={<Home/>} />
         </Routes>
         <Footer />
       </Router>
